7c6d7388c0919f1f39fd817777258eb9e56d49d8
Ing.Daron11 <daronmercadog@gmail.com>
2025-05-11 16:27:47 -0500

    doc: add documentation for users test e2e

--- test/users/users.e2e-spec.ts
+++ test/users/users.e2e-spec.ts
@@ -17,6 +17,11 @@ describe('Users (e2e)', () => {
   const originalName = 'daron';
   const updatedName = 'NuevoNombre';
 
+  // ========== LOGIN ==========
+    /**
+     * 0. Esto es un setup para iniciar sesión
+     * y obtener el token JWT para las pruebas
+     */
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -25,7 +30,6 @@ describe('Users (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
 
-    // LOGIN
     const res = await request(app.getHttpServer())
       .post('/auth/login')
       .send(loginCredentials);
@@ -36,7 +40,18 @@ describe('Users (e2e)', () => {
     const decoded: any = jwt.decode(token);
     user_id = decoded.user_id;
   });
-
+// |===============================|
+// | _____ _____ ____ _____ ____   |
+// | |_   _| ____/ ___|_   _/ ___| |
+// |   | | |  _| \___ \ | | \___ \ |
+// |   | | | |___ ___) || |  ___) ||
+// |   |_| |_____|____/ |_| |____/ |
+// |===============================|
+                               
+  /**
+   * 1. Este test verifica que el token JWT 
+   * se genere correctamente y contenga el ID de usuario
+  */
   it('/users/me (GET) – debe retornar el perfil', async () => {
     const res = await request(app.getHttpServer())
       .get('/users/me')
@@ -47,6 +62,11 @@ describe('Users (e2e)', () => {
     expect(res.body.email).toBe(loginCredentials.email);
   });
 
+  /**
+   * 2. Este test verifica que el endpoint
+   * /users/:id retorne el usuario por ID y sus credenciales
+   * (email y nombre)
+   */
   it('/users/:id (GET) – debe retornar el usuario por ID', async () => {
     const res = await request(app.getHttpServer())
       .get(`/users/${user_id}`)
@@ -57,6 +77,11 @@ describe('Users (e2e)', () => {
     expect(res.body.email).toBe(loginCredentials.email);
   });
 
+  /**
+   * 3. Este test verifica que el endpoint
+   * /users/:id aactualice el nombre del usuario
+   * y retorne el usuario actualizado
+   */
   it('/users/:id (PATCH) – actualizar nombre', async () => {
     const res = await request(app.getHttpServer())
       .patch(`/users/${user_id}`)
@@ -69,6 +94,12 @@ describe('Users (e2e)', () => {
     expect(res.body.name).toBe(updatedName);
   });
 
+  /**
+   * 4. Este test verifica que el endpoint
+   * /users/:id actualice el nombre del usuario al original
+   * y retorne el usuario actualizado dejándolo como antes
+   * (daron)
+   */
   it('/users/:id (PATCH) – revertir nombre original', async () => {
     const res = await request(app.getHttpServer())
       .patch(`/users/${user_id}`)
@@ -81,6 +112,10 @@ describe('Users (e2e)', () => {
     expect(res.body.name).toBe(originalName);
   });
 
+  /**
+   * Y ya esta vuelta es para cerrar la sesión
+   * y eliminar el token de la memoria
+   */
   afterAll(async () => {
     await app.close();
   });
