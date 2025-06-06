8f1bee8ea95cbdd1fc6b46a9375c8a0ca849c539
Ing.Daron11 <daronmercadog@gmail.com>
2025-06-06 01:01:07 -0500

    feat: all works fine

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
+  //permitir el acceso a la API desde el frontend
+  //esto es para que el frontend pueda hacer peticiones a la API
   app.enableCors({
     origin: [process.env.FRONTEND_URL,
       'http://localhost:3000'],
